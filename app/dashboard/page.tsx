775abf3aa81a1258630ad4e329e48344ba8ab25b
jneil-llorente <joseneil27@gmail.com>
2025-05-14 09:55:29 +1000

    added api webhooks to the clerk

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,21 @@
-import { syncUserWithDatabase } from "@/lib/clerk-sync";
-import prisma from "@/lib/db"
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/db";
 
 export default async function Dashboard() {
-  // Sync user data whenever they access the dashboard
-  await syncUserWithDatabase();;
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("User is not authenticated.");
+  }
+
+  const dbUser = await prisma.user.findUnique({
+    where: { clerkUserId: userId },
+  });
 
   return (
     <div>
-      <h1 className='text-3xl font-bold'>welcome, John</h1>
+      <h1 className='text-3xl font-bold'>welcome, {dbUser?.name || "User"}</h1>
       <p className='text-gray-500 mt-1'>manage your test cases and codes</p>
     </div>
-
-    
-  )
+  );
 }
