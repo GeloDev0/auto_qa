bf7be5bb232b7a212a20a17367ad597bccd4ca06
jneil-llorente <joseneil27@gmail.com>
2025-05-15 15:50:26 +1000

    added CRUD functionalities on the clerk user and sync it with the DB

--- app/( authenticated )/dashboard/page.tsx
+++ app/( authenticated )/dashboard/page.tsx
@@ -1,5 +1,13 @@
+import { checkRole } from '@/utils/roles'
+import { redirect } from 'next/navigation'
 
-export default function UserDashboard() {
+export default async function UserDashboard() {
+    // Protect the page from users who are admins
+  const isAdmin = await checkRole('admin')
+  if (isAdmin) {
+    redirect('/')
+  }
+
   return (
     <div>
       <p>This is the user dashboard. Hello User!</p>
